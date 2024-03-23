bf77a5cfe2f79d458b02134c72f6fd22be85ccda
bssbrit <brunosabrito2@gmail.com>
2024-03-23 12:35:57 -0300

    make Medalha and Medalheiro methods and attributes

--- src/Medalha.java
+++ src/Medalha.java
@@ -2,19 +2,29 @@ public class Medalha {
 
 	private int codigo;
 
-	private int tipo;d
+	private int tipo;
 
 	private boolean individual;
 
 	private String modalidade;
 
-	public Medalha(int codigo,int  tipo,boolean individual) {
+	private Atleta atleta;
+
+	public Medalha(int codigo,int  tipo,boolean individual,String modalidade) {
 		this.codigo = codigo;
 		this.tipo = tipo;
 		this.individual = individual;
+		this.modalidade = modalidade;
 	}
 	public void adicionaAtleta(Atleta atleta) {
+        this.atleta = atleta;
+	}
 
+	public int getCodigo(){
+		return this.codigo;
+	}
+	public String getModalidade(){
+		return this.modalidade;
 	}
 
 }
--- src/Medalheiro.java
+++ src/Medalheiro.java
@@ -2,17 +2,40 @@ import java.util.ArrayList;
 
 public class Medalheiro {
 
-	private Medalha[] medalha;
+	private ArrayList<Medalha> medalheiro = null;
 
 	public boolean cadastraMedalha(Medalha m) {
+		if(consultaMedalha(m.getCodigo()) == null){
+			medalheiro.add(m);
+			return true;
+		}
 		return false;
 	}
 
 	public Medalha consultaMedalha(int codigo) {
+		if(this.medalheiro.isEmpty()){
+			return null;
+		}
+		Medalha medalhaAchada;
+        for (Medalha medalha : medalheiro) {
+            medalhaAchada = medalha;
+            if (medalhaAchada.getCodigo() == codigo ) {
+                return medalhaAchada;
+            }
+        }
 		return null;
 	}
 
 	public ArrayList<Medalha> consultaMedalhas(String modalidade) {
+		ArrayList<Medalha> subMedalhas = null;
+		for(Medalha medalha : medalheiro){
+			if(medalha.getModalidade().equals(modalidade)){
+				subMedalhas.add(medalha);
+			}
+		}
+		if(!subMedalhas.isEmpty()) {
+			return subMedalhas;
+		}
 		return null;
 	}
 
